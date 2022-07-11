b6c0852b4da625bf3fe63d59f61f01779d41aa5f
masahirookubo <stmhamachiii@gmail.com>
2022-07-11 15:41:35 +0900

    微修正

--- index.ts
+++ index.ts
@@ -33,50 +33,52 @@ const divisions: Division[] = [
   },
 ];
 
-const divisionData: Array<Division> = [
-  { division_id: "hoge1", children: [] },
-  {
-    division_id: "hoge2",
-    children: [
-      { division_id: "foo1", children: [] },
-      { division_id: "foo2", children: [] },
-      {
-        division_id: "foo4",
-        children: [
-          { division_id: "fizz1", children: [] },
-          { division_id: "fizz2", children: [] },
-          { division_id: "fizz4", children: [] },
-        ],
-      },
-      {
-        division_id: "foo5",
-        children: [
-          { division_id: "fizz5", children: [] },
-          { division_id: "fizz6", children: [] },
-          {
-            division_id: "fizz7",
-            children: [
-              { division_id: "aaa1", children: [] },
-              { division_id: "aaa2", children: [] },
-              { division_id: "aaa4", children: [] },
-              { division_id: "aaa7", children: [] },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  { division_id: "hoge3", children: [] },
-  { division_id: "hoge4", children: [] },
-  { division_id: "hoge5", children: [] },
-  { division_id: "hoge8", children: [] },
-  { division_id: "hoge6", children: [] },
-];
+// const divisionData: Array<HogeNode> = [
+//   { division_id: "hoge1", children: [] },
+//   {
+//     division_id: "hoge2",
+//     children: [
+//       { division_id: "foo1", children: [] },
+//       { division_id: "foo2", children: [] },
+//       {
+//         division_id: "foo4",
+//         children: [
+//           { division_id: "fizz1", children: [] },
+//           { division_id: "fizz2", children: [] },
+//           {
+//             division_id: "fizz4",
+//             children: [{ division_id: "fizz7", children: [] }],
+//           },
+//         ],
+//       },
+//       {
+//         division_id: "foo5",
+//         children: [
+//           { division_id: "fizz5", children: [] },
+//           { division_id: "fizz6", children: [] },
+//           {
+//             division_id: "fizz7",
+//             children: [
+//               { division_id: "aaa1", children: [] },
+//               { division_id: "aaa2", children: [] },
+//               { division_id: "aaa4", children: [] },
+//               { division_id: "aaa7", children: [] },
+//             ],
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   { division_id: "hoge3", children: [] },
+//   { division_id: "hoge4", children: [] },
+//   { division_id: "hoge5", children: [] },
+//   { division_id: "hoge8", children: [] },
+//   { division_id: "hoge6", children: [] },
+// ];
 
 function findById(data: Array<Division>, division_id: string): Division {
   // TODO(okubo): 可能なら肩入れる
   let result: any;
-  let parent: any;
   data.some(iter);
   function iter(a: Division): boolean {
     if (a.division_id === division_id) {
@@ -88,77 +90,79 @@ function findById(data: Array<Division>, division_id: string): Division {
   return result;
 }
 
-console.log(findById(divisionData, "fizz5"));
+console.log(findById(divisions, "230cca4e-1e35-41c6-b7ed-f580b876312"));
 
-type HogeNode = {
-  division_id: string;
-  children: Array<HogeNode>;
-};
-var hogeNodes: Array<HogeNode> = [
-  {
-    division_id: "name1",
-    children: [
-      { division_id: "name2", children: [] },
-      { division_id: "name3", children: [] },
-      {
-        division_id: "name4",
-        children: [
-          { division_id: "name5", children: [] },
-          { division_id: "name6", children: [] },
-        ],
-      },
-      {
-        division_id: "name7",
-        children: [
-          { division_id: "name10", children: [] },
-          { division_id: "name11", children: [] },
-        ],
-      },
-    ],
-  },
-  {
-    division_id: "name8",
-    children: [{ division_id: "name9", children: [] }],
-  },
-  {
-    division_id: "name20",
-    children: [
-      {
-        division_id: "name21",
-        children: [
-          { division_id: "name22", children: [] },
-          { division_id: "name23", children: [] },
-          { division_id: "name24", children: [] },
-        ],
-      },
-    ],
-  },
-];
-
-function findParent(node: HogeNode, searchForName: any): any {
-  if (node.division_id === searchForName) {
+// type HogeNode = {
+//   division_id: string;
+//   children: Array<HogeNode>;
+// };
+// var hogeNodes: Array<HogeNode> = [
+//   {
+//     division_id: "name1",
+//     children: [
+//       { division_id: "name2", children: [] },
+//       { division_id: "name3", children: [] },
+//       {
+//         division_id: "name4",
+//         children: [
+//           { division_id: "name5", children: [] },
+//           { division_id: "name6", children: [] },
+//         ],
+//       },
+//       {
+//         division_id: "name7",
+//         children: [
+//           { division_id: "name10", children: [] },
+//           { division_id: "name11", children: [] },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     division_id: "name8",
+//     children: [{ division_id: "name9", children: [] }],
+//   },
+//   {
+//     division_id: "name20",
+//     children: [
+//       {
+//         division_id: "name21",
+//         children: [
+//           { division_id: "name22", children: [] },
+//           { division_id: "name23", children: [] },
+//           { division_id: "name24", children: [] },
+//         ],
+//       },
+//     ],
+//   },
+// ];
+//
+// NOTE(okubo): 参考：https://stackoverflow.com/questions/53277219/javascript-find-all-parents-for-element-in-tree-recursive
+// https://stackoverflow.com/questions/69824540/how-to-find-parent-on-recursive-array
+function findParent(node: Division, division_id: any): Array<Division> {
+  if (node.division_id === division_id) {
     return [];
   }
-
-  for (var treeNode of node.children) {
-    const childResult = findParent(treeNode, searchForName);
-
+  for (var child of node.children) {
+    const childResult = findParent(child, division_id);
     if (Array.isArray(childResult)) {
       console.log("------------");
       console.log(childResult);
       console.log("------------");
-      return [treeNode.division_id].concat(childResult);
+      return [child].concat(childResult);
     }
   }
+
+  return [];
 }
 
-function handleFindParent(nodes: Array<HogeNode>, target: string): HogeNode|null {
-  // loopの必要あり
-  let searchedParentNode: HogeNode | null = null;
-
+function handleFindParent(
+  nodes: Array<Division>,
+  target: string
+): Array<Division> | null {
+  let searchedParentNode: any;
   for (let node of nodes) {
-    console.log();
-    const _searchedParent = findParent(node,target);
+    const _searchedParent = findParent(node, target);
     if (_searchedParent !== undefined || _searchedParent !== null) {
       searchedParentNode = _searchedParent;
     }
@@ -166,4 +170,4 @@ function handleFindParent(nodes: Array<HogeNode>, target: string): HogeNode|null
   return searchedParentNode;
 }
 
-console.log(handleFindParent(hogeNodes, 'name22'));
+console.log(handleFindParent(divisions, "c7fe7581-9c8f-428e-8cab-5f42f16a058b"));
